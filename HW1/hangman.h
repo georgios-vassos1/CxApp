c394796347c1b893debea3a05d7d9f5970552838
georgios.vassos1 <georgios.vassos1@maersk.com>
2024-05-19 15:12:06 +0200

    Update with new function

--- HW1/hangman.h
+++ HW1/hangman.h
@@ -7,5 +7,6 @@ void print_word(char *word, char *guessed_letters);
 bool is_letter(char c);
 bool is_already_guessed(char c, char *guessed_letters);
 void play_game(char *word, const int num_rounds, int *player_wins, int *program_wins);
+void play_game_x(char **words, int totalWords, const int num_rounds, int *player_wins, int *program_wins);
 
 #endif // HANGMAN_H
